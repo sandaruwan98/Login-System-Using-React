63d4e0159550df5fdf35e2a1ffa40821fe86a4a4
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2019-12-11 17:02:27 +0530

    Images fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,19 +18,24 @@ class Home extends Component {
         </Jumbotron>
 
         <Row className="show-grid text-center">
-          <Col xs={12} sm={4} className="person-wrapper">
+          <Col xs={12} sm={3} className="person-wrapper">
             <Image src="assets/p1.jpg"  className="profile-pic" />
             <h3>Dani</h3>
             <p>Here we're limited by the time we have. Now then, let's play. Don't be afraid to make these big decisions. </p>
           </Col>
-          <Col xs={12} sm={4} className="person-wrapper">
+          <Col xs={12} sm={3} className="person-wrapper">
             <Image src="assets/p2.jpg"  className="profile-pic" />
+            <h3>Mark</h3>
+            <p>Here we're limited by the time we have. Now then, let's play. Don't be afraid to make these big decisions. </p>
+          </Col>
+          <Col xs={12} sm={3} className="person-wrapper">
+            <Image src="assets/p3.jpg"  className="profile-pic" />
             <h3>Dani</h3>
             <p>Here we're limited by the time we have. Now then, let's play. Don't be afraid to make these big decisions. </p>
           </Col>
-          <Col xs={12} sm={4} className="person-wrapper">
-            <Image src="assets/p3.jpg"  className="profile-pic" />
-            <h3>Dani</h3>
+          <Col xs={12} sm={3} className="person-wrapper">
+            <Image src="assets/p4.jpg"  className="profile-pic" />
+            <h3>Stacy</h3>
             <p>Here we're limited by the time we have. Now then, let's play. Don't be afraid to make these big decisions. </p>
           </Col>
         </Row>
